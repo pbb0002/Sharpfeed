9b5e1504ffbf25e9a6f5c03a1dfb80a05cb3f9ee
bbennett <tbbennett1@gmail.com>
2019-11-23 15:55:52 -0800

    test

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,4 +1,5 @@
 // SignUp.js
+//test change
 import React from 'react'
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
 import { firebase } from '@react-native-firebase/auth';
